b307b33028b1dc16f81f727f06c20dbcd50603e9
armandoluja <lujasaa@rose-hulman.edu>
2016-01-31 16:43:02 -0500

    updated database tables

---
+++ DatabaseBackups/backup-01-31.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 31, 2016 at 10:42 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `buildababe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `body_type_map`
+--
+
+DROP TABLE IF EXISTS `body_type_map`;
+CREATE TABLE IF NOT EXISTS `body_type_map` (
+  `val` int(11) NOT NULL,
+  `bodyType` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE IF NOT EXISTS `chat` (
+  `chatId` int(11) NOT NULL,
+  `userId1` int(11) NOT NULL,
+  `userId2` int(11) NOT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eye_color_map`
+--
+
+DROP TABLE IF EXISTS `eye_color_map`;
+CREATE TABLE IF NOT EXISTS `eye_color_map` (
+  `val` int(11) NOT NULL,
+  `color` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hair_color_map`
+--
+
+DROP TABLE IF EXISTS `hair_color_map`;
+CREATE TABLE IF NOT EXISTS `hair_color_map` (
+  `val` int(11) NOT NULL,
+  `color` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE IF NOT EXISTS `image` (
+  `userId` int(11) NOT NULL,
+  `imageId` int(11) NOT NULL,
+  `uploadDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `salt` varchar(20) NOT NULL,
+  `session_cookie` varchar(40) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `salt`, `session_cookie`, `status`) VALUES
+(1, 'tempuser', '99df00dac4e7820a4df1f49b3148b845513565e8', '00000000000000000000', '7j1ArZqn4i7jtLXLALkWV03SqGcGdOxrIhyi52wh', 0),
+(3, 'tempuser3', '3d62803337dfb60cbc438cf47803de588b14e0ed', '3LeDVgvSD12mwUcRfIhv', 'WD5rAsqfMNxL6tJUQJEjL7aT2f7cypa349hxm1GW', 0),
+(4, 'tempuser4', '1342bd71c0af757e4a0f3ddb41859b5227a43167', '55ijoT3hpQu6z37Tcc1l', 'lwZfQ2SEGvoG0OOKYud2tGe0qZ2TxQLCJTEtcQau', 0),
+(5, 'tempuser5', 'e6cc086b281f584a6dfa11fb3a248c5d201f2590', 'wkrxGL833eRw6FrHG5NY', 'foYVDNCraR3Os8AXGZwdlJ6GvyVTKVGp7oMbKJxr', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `messageId` int(11) NOT NULL,
+  `senderId` int(11) NOT NULL,
+  `receiverId` int(11) NOT NULL,
+  `timeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `content` varchar(256) NOT NULL,
+  `chatId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preference`
+--
+
+DROP TABLE IF EXISTS `preference`;
+CREATE TABLE IF NOT EXISTS `preference` (
+  `id` int(11) NOT NULL,
+  `minAge` int(11) NOT NULL DEFAULT '18',
+  `maxAge` int(11) NOT NULL DEFAULT '110',
+  `gender` int(11) NOT NULL,
+  `minHeight` int(11) NOT NULL DEFAULT '36',
+  `maxHeight` int(11) NOT NULL DEFAULT '96',
+  `oneHair` int(11) DEFAULT NULL,
+  `twoHair` int(11) DEFAULT NULL,
+  `leastHair` int(11) DEFAULT NULL,
+  `oneEye` int(11) DEFAULT NULL,
+  `twoEye` int(11) DEFAULT NULL,
+  `bodyType` int(11) DEFAULT NULL,
+  `skinTone` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` int(11) NOT NULL,
+  `fName` varchar(16) NOT NULL,
+  `lName` varchar(30) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `gender` int(11) NOT NULL,
+  `hairColor` int(11) DEFAULT NULL,
+  `eyeColor` int(11) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `bodyType` int(11) DEFAULT NULL,
+  `skinTone` int(11) DEFAULT NULL,
+  `profilePicId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved_user`
+--
+
+DROP TABLE IF EXISTS `saved_user`;
+CREATE TABLE IF NOT EXISTS `saved_user` (
+  `id` int(11) NOT NULL,
+  `savedId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `viewed`
+--
+
+DROP TABLE IF EXISTS `viewed`;
+CREATE TABLE IF NOT EXISTS `viewed` (
+  `viewerId` int(11) NOT NULL,
+  `vieweeId` int(11) NOT NULL,
+  `timeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `body_type_map`
+--
+ALTER TABLE `body_type_map`
+  ADD PRIMARY KEY (`val`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chatId`);
+
+--
+-- Indexes for table `eye_color_map`
+--
+ALTER TABLE `eye_color_map`
+  ADD PRIMARY KEY (`val`),
+  ADD UNIQUE KEY `val` (`val`);
+
+--
+-- Indexes for table `hair_color_map`
+--
+ALTER TABLE `hair_color_map`
+  ADD PRIMARY KEY (`val`);
+
+--
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`imageId`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`messageId`);
+
+--
+-- Indexes for table `preference`
+--
+ALTER TABLE `preference`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `saved_user`
+--
+ALTER TABLE `saved_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chatId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `image`
+--
+ALTER TABLE `image`
+  MODIFY `imageId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `preference`
+--
+ALTER TABLE `preference`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
